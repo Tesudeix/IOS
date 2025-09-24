dd9cc7caa9ed5060ff512d069675ae7f9c6231f3
Yukistudio <yukistudio@Yukistudios-iMac.local>
2025-09-24 17:39:54 +0800

    Initial commit

---
+++ YukiTests/YukiTests.swift
@@ -0,0 +1,16 @@
+//
+//  YukiTests.swift
+//  YukiTests
+//
+//  Created by Yukistudio on 2025.07.25.
+//
+
+import Testing
+
+struct YukiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
